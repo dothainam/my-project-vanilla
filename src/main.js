9c67f7f821c3111d9542f122def20bc3dd503784
dothainam <namdtph44461@fpt.edu.vn>
2024-01-08 14:21:10 +0700

    update

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import classes from "./main.module.css";
+
+document.querySelector('#app').innerHTML = `
+    <div class = "${classes.red}">
+        Hello world 
+    </div>
+`
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./components/*.{html,js}","./pages/*.{html,js}","./index.html"],
+  prefix: "tw-",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
